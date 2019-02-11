cd7e9ca46d9f9568044bbeeb6bb9d451f8024cbd
Fernando Paniagua <fernando.paniagua@juanxxiii.net>
2019-02-11 17:29:58 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Electronic XXIII</title>
+    </head>
+    <body>
+        <h1>Electronic XXIII</h1>
+        <?php
+        
+        ?>
+    </body>
+</html>
